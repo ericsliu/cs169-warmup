38278f8e39d317218edc0550e6097b5d738b7c00
Eric Liu <ericsliu@gmail.com>
2014-09-26 01:33:28 -0700

    Re-implemented add

--- warmup/app/models/user.rb
+++ warmup/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 
   SUCCESS = 1
+  ERR_BAD_CREDENTIALS = -1
+  ERR_USER_EXISTS = -2
+  ERR_BAD_USERNAME = -3
+  ERR_BAD_PASSWORD = -4
 
   validates_presence_of :user
   validates_uniqueness_of :user
@@ -12,7 +16,16 @@ class User < ActiveRecord::Base
     user.username = name
     user.password = pw
     user.count = 1
-    user.save
+    if user.save
+      return SUCCESS
+    else
+      if name.length > 128
+        return ERR_BAD_USERNAME
+      elsif pw.length > 128
+        return ERR_BAD_PASSWORD
+      else
+        return ERR_USER_EXISTS
+    end
   end
 
 end
