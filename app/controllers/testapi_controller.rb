6418a6cbd00c0ddd79a84ef306d083b92b483bf0
Eric Liu <ericsliu@gmail.com>
2014-09-26 14:29:55 -0700

    Fixed unit test types

--- app/controllers/testapi_controller.rb
+++ app/controllers/testapi_controller.rb
@@ -13,7 +13,9 @@ class TestapiController < ApplicationController
     split_output = output.split("\n")
     results = split_output[split_output.length - 1]
     counts = results.split(", ")
-    json = { "output" => output, "totalTests" => counts[0][0].to_i, "nrFailed" => counts[1][0].to_i }
+    totalTests = counts[0].split(" ")[0].to_i
+    nrFailed = counts[1].split(" ")[0].to_i
+    json = { "output" => output, "totalTests" => totalTests, "nrFailed" => nrFailed }
     render status: 200, json: json
   end 
 
